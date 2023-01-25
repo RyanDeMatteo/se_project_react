82697fc9468e0cd775f6fadc5017b303cb365b56
RyanDeMatteo <110950877+RyanDeMatteo@users.noreply.github.com>
2023-01-25 00:38:43 -0500

    initial design style and function

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,27 +1,24 @@
-import './ModalWithForm.css'
+import "./ModalWithForm.css";
 
-function ModalWithForm({ isOpen, name, title, buttonText, onClose, children }) {
-return (
-    <div className={
-        isOpen ? 'modal modal_type_${name}' : 'modal_type_${name} modal_hidden'
-    }
-    >
-        <div className="modal__form_container">
-            <button
-            className="modal__exit"
-            onClick={onClose}
-            type="button"
-            ></button>
+function ModalWithForm({ title, buttonText, onClose, children }) {
+  return (
+    <div className="modal">
+      <div className="modal__form-container">
+        <button
+          className="modal__close-button"
+          onClick={onClose}
+          type="button"
+        ></button>
         <h3 className="modal__form-title">{title}</h3>
-        <form className="form" name={name}>
-            {children}
-            <button className="form__submit" type="submit">
-                {buttonText}
-            </button>
+        <form className="form">
+          {children}
+          <button className="form__submit" type="submit">
+            {buttonText}
+          </button>
         </form>
-        </div>
+      </div>
     </div>
-);
+  );
 }
 
-export default ModalWithForm
+export default ModalWithForm;
