5d9c2a996bdd4fd48ea492d32a1933880ded42cf
RyanDeMatteo <110950877+RyanDeMatteo@users.noreply.github.com>
2023-02-07 00:55:39 -0500

    delete functionality

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./AddItemModal.css";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 const AddItemModal = ({ onAddItem, onCloseModal }) => {
--- src/components/DeleteConfirmModal/DeleteConfirmModal.js
+++ src/components/DeleteConfirmModal/DeleteConfirmModal.js
@@ -1,13 +1,36 @@
 import React from "react";
 import "./DeleteConfirmModal.css";
 
-const DeleteConfirmModal = () => {
+const DeleteConfirmModal = ({ onClose, handleConfirm, handleCancel }) => {
   return (
-    <>
-      <div>
-        <p>words</p>
+    <div className="delete-confirm">
+      <div className="delete-confirm__container">
+        <button
+          onClick={onClose}
+          type="button"
+          className="delete-confirm__close-button"
+          alt="close button"
+        />
+        <h3 className="delete-confirm__title">
+          Are you sure you want to delete this item? This action is
+          irreversible.
+        </h3>
+        <button
+          type="button"
+          className="delete-confirm__confirm-button"
+          onClick={handleConfirm}
+        >
+          Yes, delete item
+        </button>
+        <button
+          type="button"
+          className="delete-confirm__cancel-button"
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
